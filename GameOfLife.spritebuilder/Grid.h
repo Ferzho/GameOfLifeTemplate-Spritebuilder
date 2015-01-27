18e3a218249309f264e01ff7ba65b28d24e9e4e2
Ferzho <ferzho.lk@gmail.com>
2015-01-27 16:30:27 -0600

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.h
+++ GameOfLife.spritebuilder/Grid.h
@@ -12,5 +12,8 @@
 
 @property (nonatomic, assign) int totalAlive;
 @property (nonatomic, assign) int generation;
+@property (nonatomic, assign) int evolveStep;
+@property (nonatomic, assign) int countNeighbors;
+@property (nonatomic, assign) int updateCreatures;
 
 @end
